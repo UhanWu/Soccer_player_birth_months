ebb746b3e44d1b99f1d8da7101979b288a60e412
Yuhan Wu <jwu8438@gmail.com>
2021-06-21 20:50:23 -0400

    Stuff to commit and about

---
+++ Grab_data.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+library(rvest)
+
+raw_data <- read_html("https://en.wikipedia.org/wiki/UEFA_Euro_2020_squads")
+
+x <- raw_data %>%
+  html_elements("td:nth-child(4)") %>%
+  html_text2() %>%
+  as_tibble()
+
+write_csv(x, "birthdates.csv")
